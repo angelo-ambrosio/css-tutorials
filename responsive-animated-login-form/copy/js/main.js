2fe14ba3e2010ec340c7428103d1e5510d044879
Angelo Ambrósio <angelotosta1105@gmail.com>
2023-04-05 14:09:23 -0300

    tutorial revisto linha por linha

---
+++ responsive-animated-login-form/copy/js/main.js
@@ -0,0 +1,37 @@
+/*const inputs = document.querySelectorAll('.input')
+
+function focusFunc(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus')
+}
+
+function blurFunc(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove('focus')
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc)
+    input.addEventListener('blur', blurFunc)
+})*/
+
+const inputs = document.querySelectorAll('.input')
+
+function focusOn(){
+    let parent = this.parentNode.parentNode
+    parent.classList.add('focus')
+}
+
+function focusOff(){
+    let parent = this.parentNode.parentNode
+    if (this.value == ""){
+        parent.classList.remove('focus')
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusOn)
+    input.addEventListener('blur', focusOff)
+})
---
+++ responsive-animated-login-form/notes.md
@@ -0,0 +1,22 @@
+## BEAUTIFUL BUTTON
+
+1 - background-image: linear gradiente
+2 - to right (?)
+3 - três cores - duas iguais nas extremidades e uma no meio com tom mais brilhoso.
+4 - background-size: 200%
+5 - transition 0.5s
+6 - hover - background-position:right
+
+## ANIMATED INPUT
+
+1 - Programar o JS para fazer alterações na div pai (2x) no focus do input e quando o focus for retirado também.
+  **O objetivo é adicionar e remover a classe focus** 
+
+2 - O que deve ser alterado: ícone, posição do texto "placeholder" e a sobreposição da borda abaixo ('campo não selecionado')
+
+3 - criar as transformações no css
+    **LINHA: utilizar o after e o before brincando com o width**
+    **ÍCONE: apenas trocar a cor**
+    **"LABEL": mexer no position absolute**
+
+
